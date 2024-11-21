0de9e7a8f29095357dc8171a0116f785f81d18a7
choisimo <csimon2k0209@gmail.com>
2024-11-21 10:34:31 +0000

    init

---
+++ src/main/java/com/nodove/WSD_Assignment_03/WsdAssignment03Application.java
@@ -0,0 +1,13 @@
+package com.nodove.WSD_Assignment_03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsdAssignment03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsdAssignment03Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/nodove/WSD_Assignment_03/WsdAssignment03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nodove.WSD_Assignment_03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsdAssignment03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
